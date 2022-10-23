6ad3bb0f24c3eb369f16e52147741e5f89073dbb
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2022-10-23 13:19:07 -0300

    Implementação da rota GET ObterTodos

--- Controllers/TarefaController.cs
+++ Controllers/TarefaController.cs
@@ -27,8 +27,8 @@ namespace TrilhaApiDesafio.Controllers
         [HttpGet("ObterTodos")]
         public IActionResult ObterTodos()
         {
-            // TODO: Buscar todas as tarefas no banco utilizando o EF
-            return Ok();
+            var tarefas = _context.Tarefas.ToList();
+            return Ok(tarefas);
         }
 
         [HttpGet("ObterPorTitulo")]
