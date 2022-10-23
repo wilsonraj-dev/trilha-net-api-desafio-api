a8bf5a3c38d5b825ef06d7aa9d89ca1800da33b3
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2022-10-23 13:07:52 -0300

    Implementação da rota de DELETE

--- Controllers/TarefaController.cs
+++ Controllers/TarefaController.cs
@@ -95,6 +95,8 @@ namespace TrilhaApiDesafio.Controllers
                 return NotFound();
 
             // TODO: Remover a tarefa encontrada através do EF e salvar as mudanças (save changes)
+            _context.Tarefas.Remove(tarefaBanco);
+            _context.SaveChanges();
             return NoContent();
         }
     }
