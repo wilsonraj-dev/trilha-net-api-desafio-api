99e9a56d9a3e8791f746d061c2a213c2c295776a
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2022-10-23 12:58:44 -0300

    Criação da rota POST

--- Controllers/TarefaController.cs
+++ Controllers/TarefaController.cs
@@ -62,6 +62,11 @@ namespace TrilhaApiDesafio.Controllers
                 return BadRequest(new { Erro = "A data da tarefa não pode ser vazia" });
 
             // TODO: Adicionar a tarefa recebida no EF e salvar as mudanças (save changes)
+            if (ModelState.IsValid)
+            {
+                _context.Tarefas.Add(tarefa);
+                _context.SaveChanges();
+            }
             return CreatedAtAction(nameof(ObterPorId), new { id = tarefa.Id }, tarefa);
         }
 
